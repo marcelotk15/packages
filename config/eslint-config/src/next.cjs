ff22df632c9d9734190d6be3792af32761c8465f
Marcelo Oliveira <marcelotk_15@hotmail.com>
2023-10-04 20:17:50 -0300

    fix: eslint config

--- config/eslint-config/src/next.cjs
+++ config/eslint-config/src/next.cjs
@@ -11,10 +11,10 @@ const config = {
     'plugin:@next/next/recommended',
   ],
 
-  plugins: [...config.plugins, 'turbo'],
+  plugins: [...reactConfig.plugins, 'turbo'],
 
   rules: {
-    ...config.rules,
+    ...reactConfig.rules,
 
     '@next/next/no-html-link-for-pages': 0,
     'turbo/no-undeclared-env-vars': [
